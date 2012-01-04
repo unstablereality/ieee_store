469ea3c80d1e24bd5a17fc6a071675638cdf3e98
unstablereality <unstablereality@gmail.com>
2012-01-03 19:20:00 -0600

    FIxed Parts Kit Component listing, adding, and editing

--- app/controllers/kit_components_controller.rb
+++ app/controllers/kit_components_controller.rb
@@ -2,23 +2,29 @@ class KitComponentsController < ApplicationController
   def new
     @kit_component = KitComponent.new
   end
+  
   def create
-    @parts_kit = PartsKit.find(params[:parts_kit_id])
-    @kit_component = @parts_kit.kit_components.new()
-    unless (@kit_component.save)
-      flash.alert = "Add Component Failed!" 
+    @kit_component = KitComponent.new(params[:kit_component])
+    if (@kit_component.save)
+      redirect_to "/parts_kits/#{@kit_component.parts_kit_id}/edit"
+    else
+      flash.alert = "Save Failed!"
+      render :new
     end
-    redirect_to edit_parts_kit_path(@parts_kit)
   end
+  
   def show
     @kit_component = KitComponent.find(params[:id])
   end
+  
   def index
     @kit_components = KitComponent.all
   end
+  
   def edit
     @kit_component = KitComponent.find(params[:id])
   end
+  
   def update
     @kit_component = KitComponent.find(params[:id])
     if (@kit_component.update_attributes(params[:kit_component]))
@@ -28,14 +34,16 @@ class KitComponentsController < ApplicationController
       render :edit
     end
   end
+  
   def destroy
     @kit_component = KitComponent.find(params[:id])
+    @kit_id = @kit_component.parts_kit_id
     if (@kit_component.delete)
       flash.notice = "KitComponent Successfully Deleted"
-      redirect_to :kit_components      
+      redirect_to "/parts_kits/#{@kit_id}/edit"
     else
       flash.alert = "Destroy Failed!"
-      redirect_to :kit_components
+      redirect_to "/parts_kits/#{@kit_id}/edit"
     end
   end
 end
